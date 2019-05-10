a8e88bf9a2ca580ea88e388190f629e9c90a0579
Zhang Chenghao <40735540+johnzchgrd@users.noreply.github.com>
2019-05-10 20:58:44 +0800

    刷新周期为2s，无reset

--- display_circular.v
+++ display_circular.v
@@ -21,115 +21,176 @@
 
 
 module display_circular( 
-    input fastclk,//100MHzÊ±ÖÓ
-    input resetin,//¸´Î»ĞÅºÅ
-    output reg [3:0] select,//4¸öÖĞÑ¡ÄÄ¸öµÄÑ¡ÔñĞÅºÅ£¬Ñ¡×ó±ß4¸ö
-    output reg [7:0] hex_display//7¶Î¹ÜÏÔÊ¾Êä³ö
+    input fastclk,//100MHzæ—¶é’Ÿ
+   // input resetin,//å¤ä½ä¿¡å·
+    output reg [3:0] select,//4ä¸ªä¸­é€‰å“ªä¸ªçš„é€‰æ‹©ä¿¡å·ï¼Œé€‰å·¦è¾¹4ä¸ª
+    output reg [7:0] hex_display//7æ®µç®¡æ˜¾ç¤ºè¾“å‡º
     
-    ,output clkout//debugÓÃ
-    ,output reg [2:0]state_small
+	//,output clkdebug
+	//,output [1:0]state
     );
     wire clk1,clk2,reset;
-    reg [1:0] state_big = 0,state_big_next=0,state_small_next=0;//, state_small = 0;//´óÏÔÊ¾ÖÜÆÚ£¨clk1, ÒÆ¶¯ËÙÂÊ£©£¬Ğ¡ÏÔÊ¾ÖÜÆÚ£¨clk2, ÏÔÊ¾µ±Ç°4Î»ÊıÂë£©
-    reg [7:0] num1=0,num2=0,num3=0,num4=0;
-    reg [119:0]string={8'b11011010,8'b11111100, 8'b01100000 ,8'b11110110,
-                       8'b00000000,8'b11111100, 8'b01100110, 8'b00000000,
-                       8'b11011010,8'b11110110, 8'b00000000, 8'b01111010,
-                       8'b01111010,8'b00011100, 8'b00000000};//´¢´æÏÔÊ¾×Ö·û´®¶ÔÓ¦7¶Î¹Ü±àÂë
-    parameter SIZE = 8;//Î»¿í
+    reg [1:0]state_small=0;
+    reg [3:0] state_big=0;
     
-    freqdiv_half_Hz fd1(fastclk,1'b0,clk1);//ÖÜÆÚ 1 s
-    assign clkout = clk2;
-    freqdiv_for_mux fd2(fastclk,clk2);//Ë¢ĞÂÆµÂÊ
-    debounce2 bt1(fastclk ,resetin, reset);
-    ///// global reset -- 100MHz ////
-      always@(posedge fastclk)begin
-         if(reset)begin
-          state_big <= 4'b0;
-          state_small <= 4'b0;
-           num1 <= 8'b0;
-           num2 <= 8'b0;
-           num3 <= 8'b0;
-           num4 <= 8'b0;
-           end
-        end
-///////// unconditional state change for big cycle -- 0.5Hz ////////
-    always@(*)begin
-       if(state_big == 1 )begin//ÏÈ×ö2¸ö²âÊÔÒ»ÏÂ
-         state_big_next <= 0;
-         end
-       else
-           state_big_next <= state_big + 1;
-      end
-always@(posedge clk1)begin  state_big <= state_big_next; end
-///////// unconditional state change for samll cycle -- 2000Hz ////////////
-  always@(*)begin
-        if(state_small == 3 )begin
-          state_small_next <= 0;
-          end
-        else
-            state_small_next <= state_small + 1;
-    end
-    always@(posedge clk2)begin state_small <= state_small_next; end
-   ////// combinational part for every small cycle ///////
-   always@(*)begin
-     case(state_small)
-        0:begin select <= 4'b1000; hex_display <= num1; end
-        1:begin select <= 4'b0100; hex_display <= num2; end
-        2:begin select <= 4'b0010; hex_display <= num3; end
-        3:begin select <= 4'b0001; hex_display <= num4; end
-        default: select <= 4'b0000;
-     endcase
-   end
-  
-    
- ////// combinational part for big cycle ///////
- always@(*)begin
-   case(state_big)      
-      0:begin  num1 <= 8'b11011010;
-      num2 <= 8'b11111100;
-      num3 <= 8'b01100000;
-      num4 <= 8'b11110110;end
-      1:begin  num1 <= 8'b11111100;
-            num2 <= 8'b01100000;
-            num3 <= 8'b11110110;
-            num4 <= 8'b00000000;end
-//      2:begin  num4 <= string[11*SIZE-1:10*SIZE];
-//                  num1 <= string[14*SIZE-1:13*SIZE];
-//                  num2 <= string[13*SIZE-1:12*SIZE];
-//                  num3 <= string[12*SIZE-1:11*SIZE];end
-//      3:begin  num3 <= string[11*SIZE-1:10*SIZE];
-//                        num4 <= string[10*SIZE-1:9*SIZE];
-//                        num1 <= string[13*SIZE-1:12*SIZE];
-//                        num2 <= string[12*SIZE-1:11*SIZE];end
-//      4:begin  num2 <= string[11*SIZE-1:10*SIZE];
-//                num3 <= string[10*SIZE-1:9*SIZE];
-//                num4 <= string[9*SIZE-1:8*SIZE];
-//               num1 <= string[12*SIZE-1:11*SIZE];end
-//      5:begin  num1 <= string[11*SIZE-1:10*SIZE];
-//             num2 <= string[10*SIZE-1:9*SIZE];
-//             num3<= string[9*SIZE-1:8*SIZE];
-//            num4<= string[8*SIZE-1:7*SIZE];end
-//      6:
-//      7:
-//      8:
-//      9:
-//      10:
-//      11:
-//      12:
-//      13:
-//      14:
-//      15:
-      default:begin
-                 num1 <= 8'b0;
-                 num2 <= 8'b0;
-                 num3 <= 8'b0;
-                 num4 <= 8'b0;
-                 end
-   endcase
-   
- end
+    freqdiv_half_Hz fd1(fastclk,clk1);//å‘¨æœŸ 2 s
 
+    freqdiv_for_mux fd2(fastclk,clk2);//åˆ·æ–°é¢‘ç‡,å‘¨æœŸ 2ms
+   // debounce2 bt1(fastclk ,resetin, reset);
+	
+//assign clkdebug=clk2;
+
+always@(posedge clk1)begin
+  state_big = (state_big==15) ? 0 : (state_big+1);
+end
+
+ always@(posedge clk2)begin
+case(state_big)
+ 0:begin case(state_small)
+ 2'b00:begin select <= 4'b1000; hex_display <= 8'b11011010; state_small <= 2'b01;end
+ 2'b01:begin select <= 4'b0100; hex_display <= 8'b11111100; state_small <= 2'b10;end
+ 2'b10:begin select <= 4'b0010; hex_display <= 8'b01100000; state_small <= 2'b11;end
+ 2'b11:begin select <= 4'b0001; hex_display <= 8'b11110110; state_small <= 2'b00;end
+ default: state_small<=2'b00;//å›åˆ°åˆå§‹çŠ¶æ€
+ endcase
+ end
  
+ 1:begin case(state_small)
+ 2'b00:begin select <= 4'b1000; hex_display <= 8'b11111100; state_small <= 2'b01;end
+ 2'b01:begin select <= 4'b0100; hex_display <= 8'b01100000; state_small <= 2'b10;end
+ 2'b10:begin select <= 4'b0010; hex_display <= 8'b11110110; state_small <= 2'b11;end
+ 2'b11:begin select <= 4'b0001; hex_display <= 8'b00000000; state_small <= 2'b00;end
+ default: state_small<=2'b00;//å›åˆ°åˆå§‹çŠ¶æ€
+ endcase
+ end
  
+ 2:begin case(state_small)
+ 2'b00:begin select <= 4'b1000; hex_display <= 8'b01100000; state_small <= 2'b01;end
+ 2'b01:begin select <= 4'b0100; hex_display <= 8'b11110110; state_small <= 2'b10;end
+ 2'b10:begin select <= 4'b0010; hex_display <= 8'b00000000; state_small <= 2'b11;end
+ 2'b11:begin select <= 4'b0001; hex_display <= 8'b11111100; state_small <= 2'b00;end
+ default: state_small<=2'b00;//å›åˆ°åˆå§‹çŠ¶æ€
+ endcase
+ end
+ 
+ 3:begin case(state_small)
+ 2'b00:begin select <= 4'b1000; hex_display <= 8'b11110110; state_small <= 2'b01;end
+ 2'b01:begin select <= 4'b0100; hex_display <= 8'b00000000; state_small <= 2'b10;end
+ 2'b10:begin select <= 4'b0010; hex_display <= 8'b11111100; state_small <= 2'b11;end
+ 2'b11:begin select <= 4'b0001; hex_display <= 8'b01100110; state_small <= 2'b00;end
+ default: state_small<=2'b00;//å›åˆ°åˆå§‹çŠ¶æ€
+ endcase
+ end
+ 
+ 4:begin case(state_small)
+ 2'b00:begin select <= 4'b1000; hex_display <= 8'b00000000; state_small <= 2'b01;end
+ 2'b01:begin select <= 4'b0100; hex_display <= 8'b11111100; state_small <= 2'b10;end
+ 2'b10:begin select <= 4'b0010; hex_display <= 8'b01100110; state_small <= 2'b11;end
+ 2'b11:begin select <= 4'b0001; hex_display <= 8'b00000000; state_small <= 2'b00;end
+ default: state_small<=2'b00;//å›åˆ°åˆå§‹çŠ¶æ€
+ endcase
+ end
+ 
+ 5:begin case(state_small)
+ 2'b00:begin select <= 4'b1000; hex_display <= 8'b11111100; state_small <= 2'b01;end
+ 2'b01:begin select <= 4'b0100; hex_display <= 8'b01100110; state_small <= 2'b10;end
+ 2'b10:begin select <= 4'b0010; hex_display <= 8'b00000000; state_small <= 2'b11;end
+ 2'b11:begin select <= 4'b0001; hex_display <= 8'b11011010; state_small <= 2'b00;end
+ default: state_small<=2'b00;//å›åˆ°åˆå§‹çŠ¶æ€
+ endcase
+ end
+ 
+ 6:begin case(state_small)
+ 2'b00:begin select <= 4'b1000; hex_display <= 8'b01100110; state_small <= 2'b01;end
+ 2'b01:begin select <= 4'b0100; hex_display <= 8'b00000000; state_small <= 2'b10;end
+ 2'b10:begin select <= 4'b0010; hex_display <= 8'b11011010; state_small <= 2'b11;end
+ 2'b11:begin select <= 4'b0001; hex_display <= 8'b11110110; state_small <= 2'b00;end
+ default: state_small<=2'b00;//å›åˆ°åˆå§‹çŠ¶æ€
+ endcase
+ end
+ 
+ 7:begin case(state_small)
+ 2'b00:begin select <= 4'b1000; hex_display <= 8'b00000000; state_small <= 2'b01;end
+ 2'b01:begin select <= 4'b0100; hex_display <= 8'b11011010; state_small <= 2'b10;end
+ 2'b10:begin select <= 4'b0010; hex_display <= 8'b11110110; state_small <= 2'b11;end
+ 2'b11:begin select <= 4'b0001; hex_display <= 8'b00000000; state_small <= 2'b00;end
+ default: state_small<=2'b00;//å›åˆ°åˆå§‹çŠ¶æ€
+ endcase
+ end
+ 
+ 8:begin case(state_small)
+ 2'b00:begin select <= 4'b1000; hex_display <= 8'b11011010; state_small <= 2'b01;end
+ 2'b01:begin select <= 4'b0100; hex_display <= 8'b11110110; state_small <= 2'b10;end
+ 2'b10:begin select <= 4'b0010; hex_display <= 8'b00000000; state_small <= 2'b11;end
+ 2'b11:begin select <= 4'b0001; hex_display <= 8'b01111010; state_small <= 2'b00;end
+ default: state_small<=2'b00;//å›åˆ°åˆå§‹çŠ¶æ€
+ endcase
+ end
+ 
+ 9:begin case(state_small)
+ 2'b00:begin select <= 4'b1000; hex_display <= 8'b11110110; state_small <= 2'b01;end
+ 2'b01:begin select <= 4'b0100; hex_display <= 8'b00000000; state_small <= 2'b10;end
+ 2'b10:begin select <= 4'b0010; hex_display <= 8'b01111010; state_small <= 2'b11;end
+ 2'b11:begin select <= 4'b0001; hex_display <= 8'b01111010; state_small <= 2'b00;end
+ default: state_small<=2'b00;//å›åˆ°åˆå§‹çŠ¶æ€
+ endcase
+ end
+ 
+ 10:begin case(state_small)
+ 2'b00:begin select <= 4'b1000; hex_display <= 8'b00000000; state_small <= 2'b01;end
+ 2'b01:begin select <= 4'b0100; hex_display <= 8'b01111010; state_small <= 2'b10;end
+ 2'b10:begin select <= 4'b0010; hex_display <= 8'b01111010; state_small <= 2'b11;end
+ 2'b11:begin select <= 4'b0001; hex_display <= 8'b00011100; state_small <= 2'b00;end
+ default: state_small<=2'b00;//å›åˆ°åˆå§‹çŠ¶æ€
+ endcase
+ end
+ 
+ 11:begin case(state_small)
+ 2'b00:begin select <= 4'b1000; hex_display <= 8'b01111010; state_small <= 2'b01;end
+ 2'b01:begin select <= 4'b0100; hex_display <= 8'b01111010; state_small <= 2'b10;end
+ 2'b10:begin select <= 4'b0010; hex_display <= 8'b00011100; state_small <= 2'b11;end
+ 2'b11:begin select <= 4'b0001; hex_display <= 8'b00000000; state_small <= 2'b00;end
+ default: state_small<=2'b00;//å›åˆ°åˆå§‹çŠ¶æ€
+ endcase
+ end
+ 
+ 12:begin case(state_small)
+ 2'b00:begin select <= 4'b1000; hex_display <= 8'b01111010; state_small <= 2'b01;end
+ 2'b01:begin select <= 4'b0100; hex_display <= 8'b00011100; state_small <= 2'b10;end
+ 2'b10:begin select <= 4'b0010; hex_display <= 8'b00000000; state_small <= 2'b11;end
+ 2'b11:begin select <= 4'b0001; hex_display <= 8'b00000000; state_small <= 2'b00;end
+ default: state_small<=2'b00;//å›åˆ°åˆå§‹çŠ¶æ€
+ endcase
+ end
+ 
+ 13:begin case(state_small)
+ 2'b00:begin select <= 4'b1000; hex_display <= 8'b00011100; state_small <= 2'b01;end
+ 2'b01:begin select <= 4'b0100; hex_display <= 8'b00000000; state_small <= 2'b10;end
+ 2'b10:begin select <= 4'b0010; hex_display <= 8'b00000000; state_small <= 2'b11;end
+ 2'b11:begin select <= 4'b0001; hex_display <= 8'b11011010; state_small <= 2'b00;end
+ default: state_small<=2'b00;//å›åˆ°åˆå§‹çŠ¶æ€
+ endcase
+ end
+ 
+ 14:begin case(state_small)
+ 2'b00:begin select <= 4'b1000; hex_display <= 8'b00000000; state_small <= 2'b01;end
+ 2'b01:begin select <= 4'b0100; hex_display <= 8'b00000000; state_small <= 2'b10;end
+ 2'b10:begin select <= 4'b0010; hex_display <= 8'b11011010; state_small <= 2'b11;end
+ 2'b11:begin select <= 4'b0001; hex_display <= 8'b11111100; state_small <= 2'b00;end
+ default: state_small<=2'b00;//å›åˆ°åˆå§‹çŠ¶æ€
+ endcase
+ end
+ 
+ 15:begin case(state_small)
+ 2'b00:begin select <= 4'b1000; hex_display <= 8'b00000000; state_small <= 2'b01;end
+ 2'b01:begin select <= 4'b0100; hex_display <= 8'b11011010; state_small <= 2'b10;end
+ 2'b10:begin select <= 4'b0010; hex_display <= 8'b11111100; state_small <= 2'b11;end
+ 2'b11:begin select <= 4'b0001; hex_display <= 8'b01100000; state_small <= 2'b00;end
+ default: state_small<=2'b00;//å›åˆ°åˆå§‹çŠ¶æ€
+ endcase
+ end
+ 
+ endcase
+end
+
 endmodule

1cdfdad458f0e1a34fa7d3b199b299bd72a19955
Zhang Chenghao <40735540+johnzchgrd@users.noreply.github.com>
2019-05-10 21:12:06 +0800

    带reset
    终于成功了！！！！这么简单的一个竟然搞了一天/捂脸

--- display_circular.v
+++ display_circular.v
@@ -22,12 +22,10 @@
 
 module display_circular( 
     input fastclk,//100MHz时钟
-   // input resetin,//复位信号
+    input resetin,//复位信号
     output reg [3:0] select,//4个中选哪个的选择信号，选左边4个
     output reg [7:0] hex_display//7段管显示输出
-    
-	//,output clkdebug
-	//,output [1:0]state
+
     );
     wire clk1,clk2,reset;
     reg [1:0]state_small=0;
@@ -36,12 +34,10 @@ module display_circular(
     freqdiv_half_Hz fd1(fastclk,clk1);//周期 2 s
 
     freqdiv_for_mux fd2(fastclk,clk2);//刷新频率,周期 2ms
-   // debounce2 bt1(fastclk ,resetin, reset);
-	
-//assign clkdebug=clk2;
+    debounce2 bt1(fastclk ,resetin, reset);
 
-always@(posedge clk1)begin
-  state_big = (state_big==15) ? 0 : (state_big+1);
+always@(posedge clk1 or posedge reset)begin
+  state_big = (state_big==15 || reset) ? 0 : (state_big+1);
 end
 
  always@(posedge clk2)begin
